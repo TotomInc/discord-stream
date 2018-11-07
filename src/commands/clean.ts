7ff32c8dfca5a3351efdb7dfe9c853f2065b3b9d
Thomas Cazade <cazade.thomas@gmail.com>
2018-11-07 13:36:21 +0100

    feat(clean): add clean command to remove bot messages on specific channel

---
+++ src/commands/clean.ts
@@ -0,0 +1,17 @@
+import { Command } from '../models';
+
+module.exports = {
+  name: 'clean',
+  description: 'remove the most recent messages sent by the bot in the channel where you send the command',
+  execute: (message, args) => {
+    const channel = message.channel;
+
+    channel.fetchMessages()
+      .then((messages) => {
+        const botMessages = messages.filter((m) => m.member.id === process.env['USER_ID'] && m.deletable);
+        const deletePromises = botMessages.map((botMessage) => botMessage.delete());
+
+        return Promise.all(deletePromises);
+      });
+  },
+} as Command;
