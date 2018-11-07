83c729af405f20e4e4d3ed4aae0018ad3edd5ae5
Thomas Cazade <cazade.thomas@gmail.com>
2018-11-07 13:20:53 +0100

    feat(clear): add clear command to remove all tracks from queue

---
+++ src/commands/clear.ts
@@ -0,0 +1,23 @@
+import { Command } from '../models';
+import * as player from '../player';
+
+module.exports = {
+  name: 'clear',
+  description: 'remove all queued tracks (except the currently playing track)',
+  execute: (message, args) => {
+    const tracks = player.getQueue(message);
+    const guildVoiceConnection = message.guild.voiceConnection;
+
+    if (tracks.length <= 0) {
+      return message.reply('there are no tracks in the queue.');
+    } else if (guildVoiceConnection && guildVoiceConnection.speaking) {
+      player.removeTracks(1, tracks.length, message);
+
+      message.channel.send('Removed all tracks from the queue excepted the currently playing track.');
+    } else {
+      player.removeQueue(message);
+
+      message.channel.send('Successfully removed all tracks from the queue.');
+    }
+  },
+} as Command;
