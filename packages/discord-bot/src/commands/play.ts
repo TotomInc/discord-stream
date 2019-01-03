928234e6d66ddbb7f9b57ad97ed741832b34a1d2
Thomas Cazade <cazade.thomas@gmail.com>
2019-01-03 22:35:19 +0100

    refacto(discord-bot): better conditions and error handling for play command
    closes #8

--- packages/discord-bot/src/commands/play.ts
+++ packages/discord-bot/src/commands/play.ts
@@ -18,20 +18,37 @@ module.exports = {
 
     const query = args.join(' ');
     const isURL = utils.isURL(query);
+    /** If the query is a search-query, it is a YouTube provider by default */
     const provider = (!isURL) ? 'youtube' : utils.detectURLProvider(query);
+
+    /** If the bot doesn't have the `CONNECT` permission on this guild */
+    if (!member.permissions.has('CONNECT')) {
+      return message.channel.send(`I don't have the \`CONNECT\` permission, please create a proper role with this permission and add it to the bot.`);
+    }
+
+    /** If the bot doesn't have the `SPEAK` permission on this guild */
+    if (!member.permissions.has('SPEAK')) {
+      return message.channel.send(`I don't have the \`SPEAK\` permission, please create a proper role with this permission and add it to the bot.`);
+    }
+
+    /** If the sender is not in a voice-channel */
+    if (!member.voiceChannel || !member.voiceChannelID) {
+      return message.reply(`You are not connected to a voice-channel. You need to join a voice-channel first, then send a command to call me.`);
+    }
 
     /** If provider unefined, it means the provider is not supported */
     if (!provider) {
-      return message.channel.send(`Sorry I don\'t support this provider. Here is a list of the supported providers: ${providersList.join(', ')}`);
+      return message.channel.send(`Sorry I don\'t support this provider/service. Here is a list of the supported providers: \`${providersList.join(', ')}\``);
     }
 
     /** If we try to make a youtube search with a query too short */
     if (provider === 'youtube' && query.length < 3) {
-      return message.channel.send('The search query is too short.');
+      return message.channel.send('The search query is too short, put at least 3 characters.');
     }
 
     message.channel.send(`:mag_right: Searching on **${provider}**: \`${query}\``);
 
+    /** If the sender is in a voice-channel and the bot is not, join voice-channel */
     if (member.voiceChannelID && !client.voiceConnections.has(message.guild.id)) {
       const [joinErr, voiceConnection] = await to(member.voiceChannel.join());
 
