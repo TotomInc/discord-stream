58a8b8be4110241c23b73f83f6530b87b4758ea5
Thomas Cazade <cazade.thomas@gmail.com>
2019-01-04 16:32:07 +0100

    refacto(prefixes): use node-json-db to handle prefixes (until mongo)

--- packages/discord-bot/src/prefixes.ts
+++ packages/discord-bot/src/prefixes.ts
@@ -1,56 +1,77 @@
+import fs from 'fs-extra';
 import path from 'path';
-import fs from 'fs';
 import Discord from 'discord.js';
-
-import * as storedPrefixes from '../store/prefixes.json';
+import JSONDB from 'node-json-db';
 
 export const prefixes: Discord.Collection<string, string> = new Discord.Collection();
 export const unauthorizedPrefixes = ['$note', '$dev'];
 
-export function loadPrefixes(): Promise<Discord.Collection<string, string>> {
-  return new Promise((res, rej) => {
-    Object.entries(storedPrefixes).forEach((entry) => {
-      const guildID = entry[0];
-      const prefix = entry[1];
+const dbFilePath = path.join(__dirname, '../db/prefixes.json');
 
-      // By default, when TypeScript import a `.json` file, it imports it as a
-      // CommonJS module and have the `default` object.
-      if (guildID === 'default') {
-        return;
+/** We need to ensure that `db/prefixes.json` exists */
+fs.ensureFileSync(dbFilePath);
+
+/**
+ * If the DB file have been created, it is empty and we need to initialize a
+ * JSON object inside.
+ */
+if (fs.readFileSync(dbFilePath).toString('utf8').length <= 0) {
+  fs.writeFileSync(dbFilePath, '{}');
+}
+
+const db = new JSONDB(dbFilePath, true, true);
+
+/**
+ * Load all prefixes from the JSON DB into the Discord collection `prefixes`,
+ * return a promise which is resolved when it's done.
+ */
+export function loadPrefixes(): Promise<Discord.Collection<string, string>> {
+  const dbPrefixes = db.getData('/') as { [guildID: string ]: string};
+  const prefixesKeys = Object.keys(dbPrefixes);
+
+  return new Promise((resolve, reject) => {
+    try {
+      for (const key of prefixesKeys) {
+        const guildPrefix = dbPrefixes[key];
+
+        prefixes.set(key, guildPrefix);
       }
 
-      prefixes.set(guildID, prefix);
-    });
-
-    res(prefixes);
+      return resolve(prefixes);
+    } catch (error) {
+      return reject(error);
+    }
   });
-};
+}
 
-export function hasPrefix(message: Discord.Message) {
+/**
+ * Set a new prefix both on the `prefixes` colletion (to be used in real-time)
+ * and on the JSON DB (persistence).
+ *
+ * @param message the discord message that initiated this
+ * @param prefix the guild prefix
+ */
+export function setPrefix(message: Discord.Message, prefix: string) {
+  const guildID = message.guild.id;
+
+  db.push(`/${guildID}`, prefix, true);
+  prefixes.set(guildID, prefix!);
+}
+
+/**
+ * Check if a specific guild ID have setup a custom prefix.
+ *
+ * @param message the discord message that initiated this
+ */
+export function hasPrefix(message: Discord.Message): boolean {
   return prefixes.has(message.guild.id);
 }
 
+/**
+ * Try to get the custom prefix setup on a specific guild, if it exists.
+ *
+ * @param message the discord message that initiated this
+ */
 export function getPrefix(message: Discord.Message) {
   return prefixes.get(message.guild.id);
-}
-
-export function setPrefix(message: Discord.Message, prefix: string) {
-  return prefixes.set(message.guild.id, prefix);
-}
-
-export function savePrefixes() {
-  return new Promise((res, rej) => {
-    const prefixesPath = path.join(__dirname, '../store/prefixes.json');
-    const arrayFromMap = Array.from(prefixes.entries());
-    // @ts-ignore
-    const arrayToObject = Object.assign(...arrayFromMap.map(d => ({[d[0]]: d[1]})));
-
-    fs.writeFile(prefixesPath, JSON.stringify(arrayToObject), (err) => {
-      if (err) {
-        rej(err);
-      }
-
-      res();
-    });
-  });
 }
