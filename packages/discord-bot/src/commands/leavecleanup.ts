9f92b5931ee81302fad8b3b9034d3bd1edb08431
Thomas Cazade <cazade.thomas@gmail.com>
2019-01-08 16:01:45 +0100

    feat(leave-cleanup): add command

---
+++ packages/discord-bot/src/commands/leavecleanup.ts
@@ -0,0 +1,35 @@
+import _ from 'lodash';
+
+import { Command } from '../models';
+import * as player from '../player';
+import * as utils from '../utils';
+
+module.exports = {
+  name: 'leavecleanup',
+  description: 'removes tracks queued by absent users (users non-connected to the voice-channel)',
+  execute: async (message, args) => {
+    const voiceConnection = message.client.voiceConnections.get(message.guild.id);
+
+    if (!voiceConnection) {
+      return message.reply('I am not connected to a voice-channel.');
+    }
+
+    const queue = player.getQueue(message);
+
+    if (queue.length <= 0) {
+      return message.reply('the queue is empty.');
+    }
+
+    const voiceChannelMembers = voiceConnection.channel.members.array();
+    const filteredQueue = queue.filter((track) =>
+      voiceChannelMembers.find((member) => member.id === track.initiator.member.id)
+    );
+
+    player.replaceQueue(filteredQueue, message);
+
+    const richEmbed = utils.generateRichEmbed('Queue cleaned', message.client)
+      .setDescription('Removed tracks queued by absent users.');
+
+    message.channel.send(richEmbed);
+  },
+} as Command;
