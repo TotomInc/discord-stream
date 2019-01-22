b39cf87af911bd33745d3ce9a0bc957aef4f7cc1
Thomas Cazade <cazade.thomas@gmail.com>
2019-01-22 09:59:20 +0100

    fix(discord-bot): dynamic USER_ID env. variable for invite command
    closes #11

--- packages/discord-bot/src/commands/invite.ts
+++ packages/discord-bot/src/commands/invite.ts
@@ -1,13 +1,17 @@
 import { Command } from '../models';
 import * as utils from '../utils';
 
+const userID = process.env['USER_ID'] || '497409782072868864';
+
 module.exports = {
   name: 'invite',
   description: 'get an invitation link to add me on other servers',
   execute: (message, args) => {
     const richEmbed = utils.generateRichEmbed('Useful links', message.client)
-      .setDescription(`[Invite me](https://discordapp.com/oauth2/authorize?client_id=497409782072868864&permissions=0&scope=bot)
-      [Source-code](https://github.com/TotomInc/discord-stream)`);
+      .setDescription(`
+        [Invite me](https://discordapp.com/oauth2/authorize?client_id=${userID}&permissions=0&scope=bot)
+        [Source-code](https://github.com/TotomInc/discord-stream)
+      `);
 
     message.channel.send(richEmbed);
   },
