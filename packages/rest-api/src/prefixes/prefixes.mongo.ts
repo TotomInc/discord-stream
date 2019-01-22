3ee75a27b9dd2f917f3c95bf04f1264f6d8f69b0
Thomas Cazade <cazade.thomas@gmail.com>
2019-01-22 11:47:27 +0100

    feat(rest-api): handle put request to add/replace a prefix

--- packages/rest-api/src/prefixes/prefixes.mongo.ts
+++ packages/rest-api/src/prefixes/prefixes.mongo.ts
@@ -25,3 +25,32 @@ export async function getPrefix(guildID: string) {
 
   return await collection.findOne(query);
 }
+
+/**
+ * Insert a new entry prefix document into the collection.
+ *
+ * @param guildID the id of the discord guild
+ * @param prefix the custom-prefix for this guild
+ */
+export async function insertPrefix(guildID: string, prefix: string) {
+  const client = await connection();
+  const collection = client.db(dbName).collection<models.CustomPrefix>('prefixes');
+  const query = { guildID, prefix };
+  console.log('inserting prefix', guildID, prefix);
+  return await collection.insertOne(query);
+}
+
+/**
+ * Replace the prefix of a guild.
+ *
+ * @param guildID the id of the discord guild
+ * @param prefix the new custom-prefix for this guild
+ */
+export async function replacePrefix(guildID: string, prefix: string) {
+  const client = await connection();
+  const collection = client.db(dbName).collection<models.CustomPrefix>('prefixes');
+  const filterQuery = { guildID };
+  const query = { guildID, prefix };
+
+  return await collection.replaceOne(filterQuery, query);
+}
--- packages/rest-api/src/prefixes/prefixes.router.ts
+++ packages/rest-api/src/prefixes/prefixes.router.ts
@@ -27,6 +27,28 @@ routes.get('/:guildID', async (req, res) => {
   res.status(200).json(prefix);
 });
 
+routes.put('/:guildID', async (req, res) => {
+  const guildID = req.params['guildID'] as (string | undefined);
+  const prefix = req.query['prefix'] as (string | undefined);
+
+  if (!guildID || !prefix) {
+    return res.status(400).end();
+  }
+
+  const prefixExists = await MongoPrefixes.getPrefix(guildID);
+  const response = (!prefixExists)
+    ? await MongoPrefixes.insertPrefix(guildID, prefix)
+    : await MongoPrefixes.replacePrefix(guildID, prefix);
+
+  if (!response.result.ok) {
+    return res.status(500).end();
+  }
+
+  const insertedPrefix = await MongoPrefixes.getPrefix(guildID);
+
+  return res.status(200).json(insertedPrefix);
+});
+
 routes.get('/', async (req, res) => {
   const prefixes = await MongoPrefixes.getPrefixes();
   const prefixesArray = await prefixes.toArray();
