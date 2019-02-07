fc22db89b8a7a976887a51cb0a53392466fa1af0
Thomas Cazade <cazade.thomas@gmail.com>
2019-02-07 14:24:55 +0100

    fix: add mongoose connection options (keepAlive, newUrlParser)

--- packages/rest-api/src/index.ts
+++ packages/rest-api/src/index.ts
@@ -4,10 +4,15 @@ import bluebird from 'bluebird';
 import { config } from './config/env';
 import { app } from './config/express';
 
+const mongooseConnectionOptions: mongoose.ConnectionOptions = {
+  useNewUrlParser: true,
+  keepAlive: true,
+};
+
 // Set mongoose to use bluebird as a promise plugin
 mongoose.Promise = bluebird;
 
-mongoose.connect(config.mongoURI)
+mongoose.connect(config.mongoURI, mongooseConnectionOptions)
   .then(() => console.log(`> Mongoose connected to: ${config.mongoURI}`));
 
 app.listen(config.apiPort, () => console.log(`> Server started on :${config.apiPort}`));
