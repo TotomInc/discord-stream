c805e81d9a25ebe99ba5b462a74e6ca13f799055
Thomas Cazade <cazade.thomas@gmail.com>
2019-02-11 15:43:09 +0100

    fix: add projection to don't return _id and __v on user-model

--- packages/rest-api/src/server/user/user.model.ts
+++ packages/rest-api/src/server/user/user.model.ts
@@ -9,7 +9,7 @@ class User extends Typegoose {
 
   @staticMethod
   static getByClientID(this: ModelType<User> & typeof User, clientID: string) {
-    return this.findOne({ clientID });
+    return this.findOne({ clientID }, '-_id -__v');
   }
 
   @instanceMethod
